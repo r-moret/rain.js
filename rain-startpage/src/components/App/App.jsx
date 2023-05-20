f8805218680d3fc5bf69a2f18c951e74dc71ebcf
Rafael Moret <rafaelmoretgalan@gmail.com>
2023-05-20 13:51:05 +0200

    add conditional rendering on search bar

--- rain-startpage/src/components/App/App.jsx
+++ rain-startpage/src/components/App/App.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from "react"
 import { RainCanvas } from "../RainCanvas/RainCanvas.jsx"
 import { LinksPanel } from "../LinksPanel/LinksPanel.jsx"
 import { SearchBar } from "../SearchBar/SearchBar.jsx"
 import "./App.css"
 
 export function App() {
+    const [isSearchVisible, setIsSearchVisible] = useState(false)
+
+    const handleKeyDown = () => {
+        setIsSearchVisible(true)
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyDown)
+        return () => document.removeEventListener("keydown", handleKeyDown)
+    }, [])
+
     return (
         <>
             <RainCanvas />
-            {/* <LinksPanel /> */}
-            <SearchBar />
+            {isSearchVisible ? <SearchBar setIsSearchVisible={setIsSearchVisible}/> : <LinksPanel />}
         </>
     )
 }
--- rain-startpage/src/components/SearchBar/SearchBar.jsx
+++ rain-startpage/src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,11 @@
 import "./SearchBar.css"
 
-export function SearchBar() {
+export function SearchBar({ setIsSearchVisible }) {
     const SEARCH_QUERY = "https://www.google.com/search?q="
 
     const handleSubmit = (event) => {
         event.preventDefault()
-
+        
         const formData = Object.fromEntries(new FormData(event.target).entries())
         const search = formData.search
 
@@ -13,10 +13,16 @@ export function SearchBar() {
         window.open(`${SEARCH_QUERY}${search}`, "_self")
     }
 
+    const handleChange = (event) => {
+        if (!event.target.value) {
+            setIsSearchVisible(false)
+        }
+    }
+
     return (
         <div className="search-panel">
             <form className="search-form" onSubmit={handleSubmit} autoComplete="off">
-                <input type="text" className="search-input" name="search" autoFocus/>
+                <input type="text" className="search-input" name="search" onChange={handleChange} autoFocus/>
             </form>
             <p className="search-note">press Enter to search</p>
         </div>
